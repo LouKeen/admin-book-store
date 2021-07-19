f293a3854671cfa7a13a21ed5acb8d8bde5bf625
PieL Policarpio <patricklouie.pl@gmail.com>
2021-07-19 16:57:08 +0800

    Improved API response for Book API

--- src/main/kotlin/com/bcs/book/book/BookController.kt
+++ src/main/kotlin/com/bcs/book/book/BookController.kt
@@ -1,6 +1,7 @@
 package com.bcs.book.book
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -8,20 +9,20 @@ import org.springframework.web.bind.annotation.*
 class BookController(@Autowired private val bookService: BookService) {
     /* API for getting all the list of books stored in the database. */
     @GetMapping
-    private fun getBooks(): List<Book> {
+    private fun getBooks(): ResponseEntity<Map<String, Any>> {
         return bookService.getBooks()
     }
 
     /* API for getting a specific book stored in the database. */
     @GetMapping("/{bookId}")
-    private fun getBook(@PathVariable("bookId") bookID: Int): Book {
+    private fun getBook(@PathVariable("bookId") bookID: Int): ResponseEntity<Map<String, Any>> {
         return bookService.getBook(bookID)
     }
 
     /* API for adding a list of books in the database. */
     @PostMapping("/add")
-    private fun addBooks(@RequestBody bookModel: BookModel) {
-        bookService.addBooks(bookModel.books)
+    private fun addBooks(@RequestBody bookModel: BookModel): ResponseEntity<Map<String, Any>> {
+        return bookService.addBooks(bookModel.books)
     }
 
     /* API for updating a book in the database. */
@@ -31,19 +32,19 @@ class BookController(@Autowired private val bookService: BookService) {
         @RequestParam(required = false) name: String?,
         @RequestParam(required = false) description: String?,
         @RequestParam(required = false) categoryIds: List<Int>?
-    ) {
-        bookService.updateBook(bookID, name, description, categoryIds)
+    ): ResponseEntity<Map<String, Any>> {
+        return  bookService.updateBook(bookID, name, description, categoryIds)
     }
 
     /* API for deleting a specific book in the database. */
     @DeleteMapping("/{bookId}")
-    private fun deleteBook(@PathVariable("bookId") bookID: Int) {
-        bookService.deleteBook(bookID)
+    private fun deleteBook(@PathVariable("bookId") bookID: Int): ResponseEntity<Map<String, Any>> {
+        return bookService.deleteBook(bookID)
     }
 
     /* API for deleting a set of books in the database */
     @DeleteMapping("/delete")
-    private fun deleteBooks(@RequestParam bookIds: List<Int>) {
-        bookService.deleteBooks(bookIds)
+    private fun deleteBooks(@RequestParam bookIds: List<Int>): ResponseEntity<Map<String, Any>> {
+        return bookService.deleteBooks(bookIds)
     }
 }
--- src/main/kotlin/com/bcs/book/book/BookService.kt
+++ src/main/kotlin/com/bcs/book/book/BookService.kt
@@ -3,22 +3,34 @@ package com.bcs.book.book
 import com.bcs.book.bookCategory.BookCategory
 import com.bcs.book.bookCategory.BookCategoryRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import java.util.*
 import javax.transaction.Transactional
 
 @Service
 class BookService(@Autowired private val bookRepository: BookRepository, @Autowired private val bookCategoryRepository: BookCategoryRepository) {
-    fun getBooks(): List<Book> {
-        return bookRepository.findAll()
+    fun getBooks(): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            HttpStatus.OK to bookRepository.findAll()
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
+        }
+        return buildResponse(status, "getting", "books", result)
     }
 
-    fun getBook(bookId: Int): Book {
-        val bookById = bookRepository.findById(bookId)
-        if (!bookById.isPresent) {
-            throw IllegalArgumentException("Book with id $bookId does not exist.")
+    fun getBook(bookId: Int): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            val bookById = bookRepository.findById(bookId)
+            if (!bookById.isPresent) {
+                throw IllegalArgumentException("Book with id $bookId does not exist.")
+            }
+            HttpStatus.OK to bookById.get()
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
         }
-        return bookById.get()
+        return buildResponse(status, "getting", "book", result)
     }
 
     fun addBook(book: Book) {
@@ -29,50 +41,84 @@ class BookService(@Autowired private val bookRepository: BookRepository, @Autowi
         bookRepository.save(book)
     }
 
-    fun addBooks(books: List<Book>) {
-        books.forEach {
-            addBook(it)
+    fun addBooks(books: List<Book>): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            books.forEach {
+                addBook(it)
+            }
+            HttpStatus.CREATED to ""
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
         }
+        return buildResponse(status, "creating", "book/s", result)
     }
 
     @Transactional
-    fun updateBook(bookId: Int, name: String?, description: String?, categoryIds: List<Int>?) {
-        val bookById = bookRepository.findById(bookId).orElseThrow { throw IllegalArgumentException("Book with id $bookId does not exist.") }
+    fun updateBook(bookId: Int, name: String?, description: String?, categoryIds: List<Int>?): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            val bookById = bookRepository.findById(bookId).orElseThrow { throw IllegalArgumentException("Book with id $bookId does not exist.") }
 
-        if (!name.isNullOrBlank() && !Objects.equals(bookById.name, name)) {
-            if (bookRepository.findBooksByName(name).isPresent) {
-                throw IllegalArgumentException("Book with the name $name already exist.")
-            }
-            bookRepository.save(bookById.copy(name = name))
-        }
-
-        if(!description.isNullOrBlank()) {
-            bookRepository.save(bookById.copy(description = description))
-        }
-
-        if (!categoryIds.isNullOrEmpty()) {
-            val categoryList = mutableListOf<BookCategory>()
-
-            categoryIds.forEach {
-                val bookCategory = bookCategoryRepository.findById(it).orElseThrow { throw IllegalArgumentException("Book Category with the id $it was not found.") }
-                categoryList.add(bookCategory)
+            if (!name.isNullOrBlank() && !Objects.equals(bookById.name, name)) {
+                if (bookRepository.findBooksByName(name).isPresent) {
+                    throw IllegalArgumentException("Book with the name $name already exist.")
+                }
+                bookRepository.save(bookById.copy(name = name))
             }
 
-            if (categoryList.isNotEmpty()) {
-                bookRepository.save(bookById.copy(categories = categoryList))
+            if(!description.isNullOrBlank()) {
+                bookRepository.save(bookById.copy(description = description))
             }
+
+            if (!categoryIds.isNullOrEmpty()) {
+                val categoryList = mutableListOf<BookCategory>()
+
+                categoryIds.forEach {
+                    val bookCategory = bookCategoryRepository.findById(it).orElseThrow { throw IllegalArgumentException("Book Category with the id $it was not found.") }
+                    categoryList.add(bookCategory)
+                }
+
+                if (categoryList.isNotEmpty()) {
+                    bookRepository.save(bookById.copy(categories = categoryList))
+                }
+            }
+            HttpStatus.OK to ""
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
         }
+        return buildResponse(status, "updating", "book", result)
     }
 
-    fun deleteBook(bookId: Int) {
-        val bookById = bookRepository.existsById(bookId)
-        if (!bookById) {
-            throw IllegalArgumentException("Book with id $bookId does not exist.")
+    fun deleteBook(bookId: Int): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            val bookById = bookRepository.existsById(bookId)
+
+            if (!bookById) {
+                throw IllegalArgumentException("Book with id $bookId does not exist.")
+            }
+            HttpStatus.OK to bookRepository.deleteById(bookId)
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
         }
-        bookRepository.deleteById(bookId)
+        return buildResponse(status, "deleting", "book", result)
     }
 
-    fun deleteBooks(bookIds: List<Int>) {
-        bookIds.forEach { deleteBook(it) }
+    fun deleteBooks(bookIds: List<Int>): ResponseEntity<Map<String, Any>> {
+        val(status, result) = try {
+            bookIds.forEach { deleteBook(it) }
+            HttpStatus.OK to ""
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e
+        }
+        return buildResponse(status, "deleting", "books", result)
+    }
+
+    fun buildResponse(status: HttpStatus, action: String, item: String, result: Any) : ResponseEntity<Map<String, Any>> {
+        val stat = "status" to status
+        val mess = when(status) {
+            HttpStatus.CREATED, HttpStatus.OK -> "message" to "Successful in $action $item"
+            else -> "message" to "Failed in $action $item"
+        }
+        val res = "result" to result
+        return ResponseEntity.status(status).body(mapOf(stat, mess, res))
     }
 }
